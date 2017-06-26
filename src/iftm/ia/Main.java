0438276ec4a3139118791720247bbb15bf4e8a98
Jr Martins <juniorm2488@gmail.com>
2017-06-26 11:43:40 -0300

    Começando projeto

---
+++ src/iftm/ia/Main.java
@@ -0,0 +1,10 @@
+package iftm.ia;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/iftm/ia/Utilitarios.java
@@ -0,0 +1,5 @@
+package iftm.ia;
+
+public class Utilitarios {
+
+}
