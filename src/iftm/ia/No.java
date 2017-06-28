a021d8e1767ae8d629a30f0b483311814085495d
Jr Martins <juniorm2488@gmail.com>
2017-06-28 15:52:58 -0300

    Melorando classe

--- src/iftm/ia/No.java
+++ src/iftm/ia/No.java
@@ -8,34 +8,65 @@ public class No
 {
 	public ArrayList<No> filhos = new ArrayList<No>();
 	public static int NIVEL = 0;
+	public ArrayList<Integer> filhotes = new ArrayList<Integer>();
 	
 	public No() 
 	{
 		
 	}
 	
-	public void jogar(String movimento){
+	public void jogar(String opc)
+	{
+		int posZero = this.filhotes.indexOf(0);
+		Integer temp1 = 0, temp2 = 0;
 		
-		switch(movimento)
+		switch(opc)
 		{
-		case "cima":
-			
-			break;
-		case "baixo":
-			
-			break;
-		case "direita":
-	
-			break;
-		case "esquerda":
-			
-			break;
-		
-		
+			case "direita":
+				if(posZero != 2 && posZero != 5 && posZero != 8){
+					temp1 = filhotes.get(posZero);
+					temp2 = filhotes.get(posZero+1);
+					
+					this.filhotes.set(posZero, temp2);
+					this.filhotes.set(posZero+1, temp1);
+					
+				
+				}
+				break;
+			case "esquerda":
+				if(posZero != 0 && posZero != 3 && posZero != 6){
+					temp1 = filhotes.get(posZero);
+					temp2 = filhotes.get(posZero-1);
+					
+					this.filhotes.set(posZero, temp2);
+					this.filhotes.set(posZero-1, temp1);
+					
+					
+				}	
+				break;
+			case "cima":
+				if(posZero > 2 ){
+					temp1 = filhotes.get(posZero);
+					temp2 = filhotes.get(posZero-3);
+					
+					this.filhotes.set(posZero, temp2);
+					this.filhotes.set(posZero-3, temp1);
+					
+					
+				}
+				break;
+			case "baixo":
+				if(posZero < 6){
+					temp1 = filhotes.get(posZero);
+					temp2 = filhotes.get(posZero+3);
+					
+					this.filhotes.set(posZero, temp2);
+					this.filhotes.set(posZero+3, temp1);
+					
+					
+				}
+				break;
 		}
-		
-		
-		
 	}
 	
 	
