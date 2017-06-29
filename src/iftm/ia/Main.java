33ae0bba84af86db09607d1720b77207a00c655d
Jr Martins <juniorm2488@gmail.com>
2017-06-29 11:20:18 -0300

    Adicionando arvore

--- src/iftm/ia/Main.java
+++ src/iftm/ia/Main.java
@@ -1,10 +1,27 @@
 package iftm.ia;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Integer[] tabuleiro = {0,1,2,3,4,5,6,7,8};
+		ArrayList<Integer> array = new ArrayList<Integer>(Arrays.asList(tabuleiro));
 		
+		Collections.shuffle(array);
+		
+		No arvore = new No();
+		
+		arvore.filhotes = array;
+		
+		arvore.nivel = 1;
+		
+		arvore.criarFilhos();
+		
+		arvore.print();
 
 	}
 
--- src/iftm/ia/No.java
+++ src/iftm/ia/No.java
@@ -1,70 +1,123 @@
 package iftm.ia;
 
 import java.util.ArrayList;
-
 import iftm.ia.No;
 
 public class No 
 {
 	public ArrayList<No> filhos = new ArrayList<No>();
 	public static int NIVEL = 0;
+	public static int NOS = 0;
 	public ArrayList<Integer> filhotes = new ArrayList<Integer>();
+	public int id = 0;
+	public int nivel = 0;
 	
 	public No() 
 	{
-		
+		this.id = NOS++;
 	}
 	
-	public void jogar(String opc)
+	public void print()
+	{
+		System.out.println("No: " + this.id + " Nivel: "+ this.nivel);
+		for (int i = 0; i < filhotes.size(); i++) {
+			System.out.print(filhotes.get(i)+" ");
+			if((i+1) % 3 == 0)
+				System.out.println();
+		}
+		System.out.println("\n");
+		
+		for (int i = 0; i < filhos.size(); i++) {
+			filhos.get(i).print();
+		}
+	}
+	
+	public void criarFilhos()
+	{
+		this.jogar("direita");
+		this.jogar("esquerda");
+		this.jogar("cima");
+		this.jogar("baixo");
+		NIVEL++;
+		
+		if((this.nivel+1) < 15)
+		{
+			for (int i = 0; i < filhos.size(); i++) {
+				filhos.get(i).criarFilhos();
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private void jogar(String opc)
 	{
 		int posZero = this.filhotes.indexOf(0);
 		Integer temp1 = 0, temp2 = 0;
+		ArrayList<Integer> backup = null;
 		
 		switch(opc)
 		{
 			case "direita":
 				if(posZero != 2 && posZero != 5 && posZero != 8){
+					backup = (ArrayList<Integer>) this.filhotes.clone();
 					temp1 = filhotes.get(posZero);
 					temp2 = filhotes.get(posZero+1);
 					
-					this.filhotes.set(posZero, temp2);
-					this.filhotes.set(posZero+1, temp1);
-					
-				
+					backup.set(posZero, temp2);
+					backup.set(posZero+1, temp1);
+					No filho = new No();
+					filho.filhotes = backup;
+					filho.nivel = this.nivel +1;
+					this.filhos.add(filho);
 				}
 				break;
 			case "esquerda":
 				if(posZero != 0 && posZero != 3 && posZero != 6){
+					backup = (ArrayList<Integer>) this.filhotes.clone();
 					temp1 = filhotes.get(posZero);
 					temp2 = filhotes.get(posZero-1);
 					
-					this.filhotes.set(posZero, temp2);
-					this.filhotes.set(posZero-1, temp1);
-					
+					backup.set(posZero, temp2);
+					backup.set(posZero-1, temp1);
+					No filho = new No();
+					filho.filhotes = backup;
+					filho.nivel = this.nivel +1;
+					this.filhos.add(filho);
 					
 				}	
+				
 				break;
 			case "cima":
 				if(posZero > 2 ){
+					backup = (ArrayList<Integer>) this.filhotes.clone();
 					temp1 = filhotes.get(posZero);
 					temp2 = filhotes.get(posZero-3);
 					
-					this.filhotes.set(posZero, temp2);
-					this.filhotes.set(posZero-3, temp1);
-					
+					backup.set(posZero, temp2);
+					backup.set(posZero-3, temp1);
+					No filho = new No();
+					filho.filhotes = backup;
+					filho.nivel = this.nivel +1;
+					this.filhos.add(filho);
 					
 				}
+				
 				break;
 			case "baixo":
 				if(posZero < 6){
+					backup = (ArrayList<Integer>) this.filhotes.clone();
 					temp1 = filhotes.get(posZero);
 					temp2 = filhotes.get(posZero+3);
 					
-					this.filhotes.set(posZero, temp2);
-					this.filhotes.set(posZero+3, temp1);
-					
+					backup.set(posZero, temp2);
+					backup.set(posZero+3, temp1);
+					No filho = new No();
+					filho.filhotes = backup;
+					filho.nivel = this.nivel +1;
+					this.filhos.add(filho);
 					
 				}
+				
 				break;
 		}
 	}
